2a7f0353ed52eb8a4f6374d95d21f07b41f54602
Joons <elddy0948@gmail.com>
2021-08-22 00:22:16 +0900

    [Feat💡] : Empty, Never, Range

--- Hello Rx/Hello Rx/ViewController.swift
+++ Hello Rx/Hello Rx/ViewController.swift
@@ -8,6 +8,10 @@ class ViewController: UIViewController {
   var justObservable: Observable<Int>?
   var ofObservable: Observable<Int>?
   var fromObservable: Observable<[Int]>?
+  var emptyObservable: Observable<Void>?
+  var neverObservable: Observable<Void>?
+  var rangeObservable: Observable<Int>?
+  
   var labelText = "" {
     didSet {
       numberLabel.text = labelText
@@ -19,15 +23,19 @@ class ViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    onCompletedLabel.text = "❌"
+    onCompletedLabel.text = "onCompleted: ❌"
     justObservable = Observable.just(one)
     ofObservable = Observable.of(one, two, three)
     fromObservable = Observable.from(optional: [one, two, three])
+    
+    emptyObservable = Observable.empty()
+    neverObservable = Observable.never()
+    rangeObservable = Observable.range(start: 1, count: 10)
   }
   
   @IBAction func justAction(_ sender: UIButton) {
     guard let justObservable = justObservable else { return }
-    onCompletedLabel.text = "❌"
+    onCompletedLabel.text = "onCompleted: ❌"
     labelText = ""
     
     justObservable
@@ -36,14 +44,14 @@ class ViewController: UIViewController {
       },
       onCompleted: { [weak self] in
         DispatchQueue.main.async {
-          self?.onCompletedLabel.text = "✅"
+          self?.onCompletedLabel.text = "onCompleted: ✅"
         }
       })
       .disposed(by: disposeBag)
   }
   
   @IBAction func ofAction(_ sender: UIButton) {
-    onCompletedLabel.text = "❌"
+    onCompletedLabel.text = "onCompleted: ❌"
     labelText = ""
     guard let ofObservable = ofObservable else { return }
     ofObservable
@@ -52,14 +60,14 @@ class ViewController: UIViewController {
       },
       onCompleted: { [weak self] in
         DispatchQueue.main.async {
-          self?.onCompletedLabel.text = "✅"
+          self?.onCompletedLabel.text = "onCompleted: ✅"
         }
       })
       .disposed(by: disposeBag)
   }
   
   @IBAction func fromAction(_ sender: UIButton) {
-    onCompletedLabel.text = "❌"
+    onCompletedLabel.text = "onCompleted: ❌"
     labelText = ""
     guard let fromObservable = fromObservable else { return }
     fromObservable
@@ -68,7 +76,55 @@ class ViewController: UIViewController {
       },
       onCompleted: { [weak self] in
         DispatchQueue.main.async {
-          self?.onCompletedLabel.text = "✅"
+          self?.onCompletedLabel.text = "onCompleted: ✅"
+        }
+      })
+      .disposed(by: disposeBag)
+  }
+  
+  @IBAction func emptyAction(_ sender: UIButton) {
+    onCompletedLabel.text = "onCompleted: ❌"
+    labelText = ""
+    
+    guard let emptyObservable = emptyObservable else { return }
+    
+    emptyObservable
+      .subscribe(onCompleted: { [weak self] in
+        DispatchQueue.main.async {
+          self?.onCompletedLabel.text = "onCompleted: ✅"
+        }
+      })
+      .disposed(by: disposeBag)
+  }
+  
+  @IBAction func neverAction(_ sender: UIButton) {
+    onCompletedLabel.text = "onCompleted: ❌"
+    labelText = ""
+    
+    guard let neverObservable = neverObservable else { return }
+    
+    neverObservable
+      .subscribe(onCompleted: { [weak self] in
+        DispatchQueue.main.async {
+          self?.onCompletedLabel.text = "onCompleted: ✅"
+        }
+      })
+      .disposed(by: disposeBag)
+  }
+  
+  @IBAction func rangeOneToTenAction(_ sender: UIButton) {
+    onCompletedLabel.text = "onCompleted: ❌"
+    labelText = ""
+    
+    guard let rangeObservable = rangeObservable else { return }
+    
+    rangeObservable
+      .subscribe(onNext: { [weak self] number in
+        self?.labelText.append("\(number)")
+      },
+      onCompleted: { [weak self] in
+        DispatchQueue.main.async {
+          self?.onCompletedLabel.text = "onCompleted: ✅"
         }
       })
       .disposed(by: disposeBag)
