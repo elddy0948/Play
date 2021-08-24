b7c89d2cc25bb58b0aabfc6ceff78e1ffe6eb3ee
Joons <elddy0948@gmail.com>
2021-08-24 14:10:23 +0900

    [Feat💡] : PublishRelay 구현

--- RxRelayPractice/RxRelayPractice/ViewController.swift
+++ RxRelayPractice/RxRelayPractice/ViewController.swift
@@ -1,19 +1,37 @@
-//
-//  ViewController.swift
-//  RxRelayPractice
-//
-//  Created by 김호준 on 2021/08/24.
-//
-
 import UIKit
+import RxSwift
+import RxRelay
 
 class ViewController: UIViewController {
-
+  
+  //MARK: - Outlets
+  @IBOutlet weak var publishRelayLabel: UILabel!
+  @IBOutlet weak var firstSubscriberLabel: UILabel!
+  
+  let disposeBag = DisposeBag()
+  var number = 1
+  
+  let publishRelay = PublishRelay<Int>()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
   }
-
-
+  
+  @IBAction func onNextAction(_ sender: UIButton) {
+    publishRelay.accept(number)
+    publishRelayLabel.text?.append("\(number)")
+    number += 1
+  }
+  
+  @IBAction func firstSubscribeAction(_ sender: UIButton) {
+    sender.isEnabled = false
+    firstSubscriberLabel.backgroundColor = .systemBlue
+    firstSubscriberLabel.text = "1)"
+    publishRelay
+      .subscribe(onNext: { [weak self] number in
+        self?.firstSubscriberLabel.text?.append("\(number)")
+      })
+      .disposed(by: disposeBag)
+  }
 }
 
