bfc7d7e5cb6d9f33fa119e063dc1b2a7b809ade2
Joons <elddy0948@gmail.com>
2021-08-25 15:29:01 +0900

    [Feat💡] : Create User model

---
+++ TodayCaffeine/TodayCaffeine/User.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+struct User {
+  let name: String
+  let americano: Int = 0
+  let latte: Int = 0
+  let redbull: Int = 0
+  let hotsix: Int = 0
+  let monster: Int = 0
+  let totalMg: Int = 0
+}
