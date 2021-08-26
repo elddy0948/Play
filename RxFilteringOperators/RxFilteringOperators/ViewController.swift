b271650210fbcbacab25be59c14dfe16fe128455
Joons <elddy0948@gmail.com>
2021-08-27 01:29:26 +0900

    [Feat] : Ignore Elements 구현

--- RxFilteringOperators/RxFilteringOperators/ViewController.swift
+++ RxFilteringOperators/RxFilteringOperators/ViewController.swift
@@ -1,19 +1,46 @@
-//
-//  ViewController.swift
-//  RxFilteringOperators
-//
-//  Created by 김호준 on 2021/08/26.
-//
-
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
+  
+  //MARK: - IBOutlets
+  @IBOutlet weak var publishSubjectLabel: UILabel!
+  @IBOutlet weak var firstSubscriberLabel: UILabel!
+  
+  private let publishSubject = PublishSubject<Int>()
+  private let disposeBag = DisposeBag()
+  
+  private var number = 1
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    publishSubjectLabel.text = ""
+    firstSubscriberLabel.text = ""
   }
-
-
+  
+  //MARK: - IBActions
+  @IBAction func firstSubscribeAction(_ sender: UIButton) {
+    firstSubscriberLabel.text = "1) "
+    publishSubject
+      .ignoreElements()
+      .subscribe(onNext: { [weak self] number in
+        self?.firstSubscriberLabel.text?.append("\(number)")
+      }, onCompleted: { [weak self] in
+        self?.firstSubscriberLabel.text?.append("✅")
+      })
+      .disposed(by: disposeBag)
+    sender.isEnabled = false
+  }
+  
+  @IBAction func onNextAction(_ sender: UIButton) {
+    publishSubject.onNext(number)
+    publishSubjectLabel.text?.append("\(number)")
+    number += 1
+  }
+  
+  @IBAction func onCompletedAction(_ sender: UIButton) {
+    publishSubject.onCompleted()
+    publishSubjectLabel.text?.append("✅")
+  }
 }
 
