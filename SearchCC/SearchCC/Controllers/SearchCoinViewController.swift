b9af3328c9dafc0c1659a21fc9d69d8156cde867
Joons <elddy0948@gmail.com>
2021-09-16 14:52:40 +0900

    [Feat] SearchCryptoView 구현

--- SearchCC/SearchCC/Controllers/SearchCoinViewController.swift
+++ SearchCC/SearchCC/Controllers/SearchCoinViewController.swift
@@ -2,19 +2,36 @@ import UIKit
 
 class SearchCoinViewController: UIViewController {
   
+  private var searchCryptoView: SearchCryptoView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     configureSearchController()
+    configureSearchCryptoView()
   }
   
   private func configureSearchController() {
     let searchController = UISearchController(searchResultsController: nil)
     searchController.searchBar.placeholder = "Enter Coin ex)bitcoin"
     searchController.hidesNavigationBarDuringPresentation = false
+    searchController.searchBar.autocapitalizationType = .none
+    searchController.searchBar.autocorrectionType = .no
     
     navigationItem.searchController = searchController
     navigationItem.title = "Search"
     navigationItem.hidesSearchBarWhenScrolling = false
+  }
+  
+  private func configureSearchCryptoView() {
+    searchCryptoView = SearchCryptoView()
+    view.addSubview(searchCryptoView)
+    
+    NSLayoutConstraint.activate([
+      searchCryptoView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      searchCryptoView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+      searchCryptoView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+      searchCryptoView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+    ])
   }
 }
 
