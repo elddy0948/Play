55c6c0ea6bba68b71955642dfaf33fd525807cf3
Joons <elddy0948@gmail.com>
2022-02-20 23:56:53 +0900

    Setup AddTodo View Controller UI

--- RxGoodList/RxGoodList/Controllers/AddTodoViewController.swift
+++ RxGoodList/RxGoodList/Controllers/AddTodoViewController.swift
@@ -1,8 +1,84 @@
 import UIKit
 
 class AddTodoViewController: UIViewController {
+  //MARK: - Views
+  lazy var segmentedControl: UISegmentedControl = {
+    let segmentedControl = UISegmentedControl(items: ["High", "Medium", "Low"])
+    return segmentedControl
+  }()
+  
+  lazy var textField: UITextField = {
+    let textField = UITextField()
+    textField.leftViewMode = .always
+    textField.leftView = UIView()
+    textField.autocorrectionType = .no
+    textField.autocapitalizationType = .none
+    textField.layer.borderWidth = 1.0
+    textField.layer.borderColor = UIColor.systemGray.cgColor
+    return textField
+  }()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    view.backgroundColor = .systemRed
+    view.backgroundColor = .systemBackground
+    setupRightBarButton()
+    setupSegmentedControl()
+    setupTextField()
+    layout()
+  }
+  
+  @objc func didTappedSaveButton() {
+    
+  }
+}
+
+//MARK: - UI setup / Layout
+extension AddTodoViewController {
+  private func setupRightBarButton() {
+    let addButton = UIBarButtonItem(
+      barButtonSystemItem: .save, target: self,
+      action: #selector(didTappedSaveButton)
+    )
+    
+    navigationItem.setRightBarButton(addButton, animated: true)
+  }
+  
+  private func setupSegmentedControl() {
+    view.addSubview(segmentedControl)
+    segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+  }
+  
+  private func setupTextField() {
+    view.addSubview(textField)
+    textField.translatesAutoresizingMaskIntoConstraints = false
+  }
+  
+  private func layout() {
+    let safeAreaLayoutGuide = view.safeAreaLayoutGuide
+    let padding: CGFloat = 8
+    
+    NSLayoutConstraint.activate([
+      segmentedControl.topAnchor.constraint(
+        equalTo: safeAreaLayoutGuide.topAnchor,
+        constant: padding
+      ),
+      segmentedControl.centerXAnchor.constraint(
+        equalTo: safeAreaLayoutGuide.centerXAnchor
+      ),
+      textField.centerXAnchor.constraint(
+        equalTo: safeAreaLayoutGuide.centerXAnchor
+      ),
+      textField.centerYAnchor.constraint(
+        equalTo: safeAreaLayoutGuide.centerYAnchor
+      ),
+      textField.leadingAnchor.constraint(
+        equalTo: safeAreaLayoutGuide.leadingAnchor,
+        constant: padding * 2
+      ),
+      textField.trailingAnchor.constraint(
+        equalTo: safeAreaLayoutGuide.trailingAnchor,
+        constant: -(padding * 2)
+      )
+    ])
   }
 }
--- RxGoodList/RxGoodList/Coordinators/TodoListCoordinator.swift
+++ RxGoodList/RxGoodList/Coordinators/TodoListCoordinator.swift
@@ -24,6 +24,9 @@ extension TodoListCoordinator: TodoListViewControllerDelegate {
   func didTappedAddButton(_ viewController: TodoListViewController) {
     guard let router = router as? TodoListRouter else { return }
     let addTodoViewController = AddTodoViewController()
-    router.presentModally(addTodoViewController, animated: true, onDismissed: nil)
+    let navigationController = UINavigationController( // Just for navigation bar
+      rootViewController: addTodoViewController
+    )
+    router.presentModally(navigationController, animated: true, onDismissed: nil)
   }
 }
