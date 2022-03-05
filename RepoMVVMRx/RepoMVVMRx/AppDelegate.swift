2780586219a63569a8995a6fb778d0e5910e1848
Joons <elddy0948@gmail.com>
2022-03-05 12:33:47 +0900

    [Refactor] : Setup AppDelegate

--- RepoMVVMRx/RepoMVVMRx/AppDelegate.swift
+++ RepoMVVMRx/RepoMVVMRx/AppDelegate.swift
@@ -10,7 +10,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
     window?.makeKeyAndVisible()
     window?.backgroundColor = .systemBackground
-    window?.rootViewController = ViewController()
+    
+    let dependencies = ReposViewModel.Dependencies(networking: NetworkingApi())
+    let viewModel = ReposViewModel(dependencies: dependencies)
+    let viewController = ReposViewController(viewModel: viewModel)
+    
+    window?.rootViewController = viewController
     
     return true
   }
