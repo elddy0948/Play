f02e81bdf7f72b2cc4fbb90594bc8a8a6adfb14e
Joons <elddy0948@gmail.com>
2022-03-05 15:46:35 +0900

    [Create] : FirestoreApi

---
+++ FirestorePagingExample/FirestorePagingExample/Networking/FirestoreApi.swift
@@ -0,0 +1,53 @@
+import Foundation
+import Firebase
+import FirebaseFirestoreSwift
+
+final class FirestoreApi {
+  
+  private let fireStore = Firestore.firestore()
+  private var query: Query? = nil
+  
+  func fetchGreetings(completion: @escaping ([CellModel]) -> Void) {
+    let collection = fireStore.collection("cells")
+    let requestQuery: Query
+    
+    if let query = query {
+      //There is last query
+      requestQuery = query
+    } else {
+      //It's First query request
+      requestQuery = collection
+        .order(by: "number")
+        .limit(to: 25)
+    }
+    
+    requestQuery.addSnapshotListener({ [weak self] (snapshot, error) in
+      guard let self = self else { return }
+      
+      guard let snapshot = snapshot,
+            let lastSnapshot = snapshot.documents.last else {
+              completion([])
+              return
+            }
+      
+      let next = collection
+        .order(by: "number")
+        .limit(to: 25)
+        .start(afterDocument: lastSnapshot)
+      
+      //Set next query
+      self.query = next
+      
+      let cellModels = snapshot.documents.map({ document -> CellModel in
+        do {
+          let cellModel = try document.data(as: CellModel.self)
+          return cellModel
+        } catch {
+          return CellModel.empty
+        }
+      })
+      
+      completion(cellModels)
+    })
+  }
+}
