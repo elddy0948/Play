eb1ccc7e1dce4eb8979e28410533d263926e65bb
Joons <elddy0948@gmail.com>
2022-03-05 14:26:44 +0900

    Setup Project and Firestore

--- FirestorePagingExample/FirestorePagingExample/ViewController.swift
+++ FirestorePagingExample/FirestorePagingExample/ViewController.swift
@@ -1,19 +1,10 @@
-//
-//  ViewController.swift
-//  FirestorePagingExample
-//
-//  Created by 김호준 on 2022/03/05.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    view.backgroundColor = .systemOrange
   }
-
-
 }
 
