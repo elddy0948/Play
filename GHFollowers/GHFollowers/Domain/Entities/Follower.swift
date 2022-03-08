46afbee13acd28e4072b3ea101abbf7a3fea4dab
Joons <elddy0948@gmail.com>
2022-03-08 16:18:19 +0900

    Domain의 Entities 구현
    - Follower
    - FollowerQuery
    - FollowersPage

---
+++ GHFollowers/GHFollowers/Domain/Entities/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by 김호준 on 2022/03/08.
+//
+
+import Foundation
+
+struct Follower {
+  let login: String
+  let avatarURL: String
+}
---
+++ GHFollowers/GHFollowers/Domain/Entities/FollowersPage.swift
@@ -0,0 +1,13 @@
+//
+//  FollowersPage.swift
+//  GHFollowers
+//
+//  Created by 김호준 on 2022/03/08.
+//
+
+import Foundation
+
+struct FollowersPage {
+  let page: Int
+  let followers: [Follower]
+}
