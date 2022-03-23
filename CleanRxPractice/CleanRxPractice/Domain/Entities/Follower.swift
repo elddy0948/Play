c440963cad19c0422a78f3e9f00eb10aef92e6c8
Joons <elddy0948@gmail.com>
2022-03-23 16:23:16 +0900

    [Refactor] : CodingKey 추가

--- CleanRxPractice/CleanRxPractice/Domain/Entities/Follower.swift
+++ CleanRxPractice/CleanRxPractice/Domain/Entities/Follower.swift
@@ -3,4 +3,9 @@ import Foundation
 public struct Follower: Decodable {
   let login: String
   let avatarURL: String?
+  
+  enum CodingKeys: String, CodingKey {
+    case login
+    case avatarURL = "avatar_url"
+  }
 }
