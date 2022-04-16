3d30e57547bb308a181ab88757da41752d2f2d5a
Joons <elddy0948@gmail.com>
2022-04-16 12:05:31 +0900

    proj init

---
+++ LOLFavorites/LOLFavorites/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LOLFavorites
+//
+//  Created by 김호준 on 2022/04/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
