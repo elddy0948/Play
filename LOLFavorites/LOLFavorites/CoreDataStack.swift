699605c2a53e1d17ab4a7291d58c61280289dcec
Joons <elddy0948@gmail.com>
2022-04-16 12:34:21 +0900

    Create Core Data Stack

--- LOLFavorites/LOLFavorites/CoreDataStack.swift
+++ LOLFavorites/LOLFavorites/CoreDataStack.swift
@@ -2,5 +2,34 @@ import Foundation
 import CoreData
 
 final class CoreDataStack {
+  private let entityName: String
   
+  private lazy var container: NSPersistentContainer = {
+    let container = NSPersistentContainer(name: self.entityName)
+    container.loadPersistentStores(completionHandler: { (description, error) in
+      if let error = error {
+        print("Can't load persistent stores \(error)")
+        return
+      }
+      print(description)
+    })
+    return container
+  }()
+  
+  lazy var managedObjectContext: NSManagedObjectContext = {
+    return container.viewContext
+  }()
+  
+  init(entityName: String) {
+    self.entityName = entityName
+  }
+  
+  func saveContext() {
+    guard managedObjectContext.hasChanges else { return }
+    do {
+      try managedObjectContext.save()
+    } catch let error as NSError {
+      print("Failed to save \(error), \(error.userInfo)")
+    }
+  }
 }
