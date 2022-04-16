cad323475c6c0879cd0e088ad8b6469f0d7aaa09
Joons <elddy0948@gmail.com>
2022-04-16 13:48:40 +0900

    connect cell with viewcontroller

--- LOLFavorites/LOLFavorites/Cells/AllChampCell.swift
+++ LOLFavorites/LOLFavorites/Cells/AllChampCell.swift
@@ -1,9 +1,20 @@
 import UIKit
 
+protocol AllChampCellDelegate: AnyObject {
+  func addFavorite(_ champion: Champion?)
+}
+
 final class AllChampCell: UITableViewCell {
   @IBOutlet weak var chapNameLabel: UILabel!
+  var champion: Champion?
+  weak var delegate: AllChampCellDelegate?
+  
+  func configure(_ champ: Champion) {
+    chapNameLabel.text = champ.name
+    self.champion = champ
+  }
   
   @IBAction func addFavorite(_ sender: UIButton) {
-    
+    delegate?.addFavorite(self.champion)
   }
 }
