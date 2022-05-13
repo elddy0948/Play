cf68cdaefe357636cb2aefd2ce4b3285d5c0d879
Joons <elddy0948@gmail.com>
2022-05-13 13:03:30 +0900

    project init

---
+++ MetalPractice01/MetalPractice01/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MetalPractice01
+//
+//  Created by 김호준 on 2022/05/13.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
