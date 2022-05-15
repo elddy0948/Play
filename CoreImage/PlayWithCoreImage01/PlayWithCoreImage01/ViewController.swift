3b44a6eba78db16496c70d5d635593c93d264231
Joons <elddy0948@gmail.com>
2022-05-15 15:38:33 +0900

    CIFilter

--- CoreImage/PlayWithCoreImage01/PlayWithCoreImage01/ViewController.swift
+++ CoreImage/PlayWithCoreImage01/PlayWithCoreImage01/ViewController.swift
@@ -1,15 +1,11 @@
-//
-//  ViewController.swift
-//  PlayWithCoreImage01
-//
-//  Created by 김호준 on 2022/05/15.
-//
-
 import UIKit
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 class ViewController: UIViewController {
   
   @IBOutlet weak var imageView: UIImageView!
+  var ciimage: CIImage?
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -19,12 +15,21 @@ class ViewController: UIViewController {
       CIImageOption.applyOrientationProperty: true
     ]
     
-    guard let url = Bundle.main.url(forResource: "IMG_7577", withExtension: "jpeg"),
-          let ciimage = CIImage(contentsOf: url, options: ciImageOptions) else {
+    guard let url = Bundle.main.url(forResource: "IMG_7577", withExtension: "jpeg") else {
       return
     }
+    ciimage = CIImage(contentsOf: url, options: ciImageOptions)
     
-    imageView.image = UIImage(ciImage: ciimage.transformed(by: CGAffineTransform(scaleX: 0.125, y: 0.125)))
+    guard let image = ciimage?.transformed(by: CGAffineTransform(scaleX: 0.125, y: 0.125)) else { return }
+    imageView.image = UIImage(ciImage: image)
+  }
+  
+  @IBAction func filterButtonAction(_ sender: UIButton) {
+    let filter = CIFilter.vibrance()
+    filter.amount = 0.9
+    filter.inputImage = ciimage?.transformed(by: CGAffineTransform(scaleX: 0.125, y: 0.125))
+    guard let output = filter.outputImage else { return }
+    imageView.image = UIImage(ciImage: output)
   }
 }
 
