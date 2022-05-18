97efe1a952d0f77425f763201dae9ee4116fa899
Joons <elddy0948@gmail.com>
2022-05-18 15:30:11 +0900

    Feat PostsViewController

--- CoreDataMVVM/CoreDataMVVM/Application/Scenes/AllPosts/PostsViewController.swift
+++ CoreDataMVVM/CoreDataMVVM/Application/Scenes/AllPosts/PostsViewController.swift
@@ -1,15 +1,39 @@
 import UIKit
+import RxSwift
 
 final class PostsViewController: UIViewController {
   private let coreDataUseCaseProvider: UseCaseProvider
+  private let viewModel: PostsViewModel
+  private var triggerSubject = PublishSubject<Void>()
+  private let bag = DisposeBag()
   
   init() {
-    coreDataUseCaseProvider = CDUseCaseProvider()
+    self.coreDataUseCaseProvider = CDUseCaseProvider()
+    self.viewModel = PostsViewModel(useCase: coreDataUseCaseProvider.makePostsUseCase())
     super.init(nibName: nil, bundle: nil)
   }
   
   required init?(coder: NSCoder) {
-    coreDataUseCaseProvider = CDUseCaseProvider()
+    self.coreDataUseCaseProvider = CDUseCaseProvider()
+    self.viewModel = PostsViewModel(useCase: coreDataUseCaseProvider.makePostsUseCase())
     super.init(coder: coder)
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    bindViewModel()
+    
+    triggerSubject.onNext(())
+  }
+  
+  private func bindViewModel() {
+    let input = PostsViewModel.Input(trigger: triggerSubject.asObservable())
+    let output = viewModel.transform(input)
+    output.posts
+      .observe(on: MainScheduler.instance)
+      .subscribe(onNext: { posts in
+        print(posts)
+      })
+      .disposed(by: bag)
   }
 }
