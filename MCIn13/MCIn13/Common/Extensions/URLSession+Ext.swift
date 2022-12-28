4b416ea5420a9b762b416fdd8397f24b6934d235
Joons <elddy0948@gmail.com>
2022-12-29 01:50:07 +0900

    [Feat] : Create download extension with async/await

--- MCIn13/MCIn13/Common/Extensions/URLSession+Ext.swift
+++ MCIn13/MCIn13/Common/Extensions/URLSession+Ext.swift
@@ -29,4 +29,32 @@ extension URLSession {
         onCancel()
       })
   }
+  
+  func download(from url: URL) async throws -> (URL, URLResponse) {
+    return try await download(for: URLRequest(url: url))
+  }
+  
+  func download(for request: URLRequest) async throws -> (URL, URLResponse) {
+    var downloadTask: URLSessionDownloadTask?
+    let onCancel = { downloadTask?.cancel() }
+    
+    return try await withTaskCancellationHandler(
+      operation: {
+        try await withCheckedThrowingContinuation({ continuation in
+          downloadTask = self.downloadTask(
+            with: request,
+            completionHandler: { url, response, error in
+              guard let url = url,
+                    let response = response else {
+                let error = error ?? URLError(.badServerResponse)
+                return continuation.resume(throwing: error)
+              }
+              continuation.resume(returning: (url, response))
+            })
+          downloadTask?.resume()
+        })
+      }, onCancel: {
+        onCancel()
+      })
+  }
 }
