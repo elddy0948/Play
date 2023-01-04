21157e0810b2c7db1acfb706eff3a28ecdd33cd7
Joons <elddy0948@gmail.com>
2023-01-05 01:48:32 +0900

    create xkcdViewer

---
+++ xkcdViewer/xkcdViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  xkcdViewer
+//
+//  Created by 김호준 on 2023/01/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
