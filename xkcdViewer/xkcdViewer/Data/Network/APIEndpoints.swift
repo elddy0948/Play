574849a983b6c8f88a690f4726fcd2e74c22823e
Joons <elddy0948@gmail.com>
2023-01-09 22:28:39 +0900

    [xkcdViewer] : URLSession+Ext

---
+++ xkcdViewer/xkcdViewer/Data/Network/APIEndpoints.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+final class APIEndpoints {
+  private let baseURL = "https://xkcd.com/info.0.json"
+  
+  static func fetch(num: Int) {
+    
+  }
+}
---
+++ xkcdViewer/xkcdViewer/Extensions/URLSession+Ext.swift
@@ -0,0 +1,57 @@
+import Foundation
+
+@available (iOS, deprecated: 15.0, message: "this can be used earlier than iOS 15")
+extension URLSession {
+  func data(from url: URL) async throws -> (Data, URLResponse) {
+    return try await data(for: URLRequest(url: url))
+  }
+  
+  func data(for request: URLRequest) async throws -> (Data, URLResponse) {
+    var dataTask: URLSessionDataTask?
+    let onCancel = { dataTask?.cancel() }
+    
+    return try await withTaskCancellationHandler(operation: {
+      try await withCheckedThrowingContinuation({ continuation in
+        dataTask = self.dataTask(
+          with: request,
+          completionHandler: { (data, response, error) in
+            guard let data = data,
+                  let response = response else {
+              let error = error ?? URLError(.badServerResponse)
+              return continuation.resume(throwing: error)
+            }
+            continuation.resume(returning: (data, response))
+          })
+        
+        dataTask?.resume()
+      })
+    }, onCancel: { onCancel() })
+  }
+  
+  func download(from url: URL) async throws -> (URL, URLResponse) {
+    return try await download(for: URLRequest(url: url))
+  }
+  
+  func download(for request: URLRequest) async throws -> (URL, URLResponse) {
+    var downloadTask: URLSessionDownloadTask?
+    let onCancel = { downloadTask?.cancel() }
+    
+    return try await withTaskCancellationHandler(operation: {
+      try await withCheckedThrowingContinuation({ continuation in
+        downloadTask = self.downloadTask(
+          with: request,
+          completionHandler: { (url, response, error) in
+            guard let url = url,
+                  let response = response else {
+              let error = error ?? URLError(.badServerResponse)
+              return continuation.resume(throwing: error)
+            }
+            continuation.resume(returning: (url, response))
+          })
+        
+        downloadTask?.resume()
+      })
+    }, onCancel: { onCancel() })
+  }
+}
+
