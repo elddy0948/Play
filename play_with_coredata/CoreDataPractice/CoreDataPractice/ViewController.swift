061d18eef86b7865a2103025b939b3e7d2311dc7
Joons <elddy0948@gmail.com>
2023-01-12 00:25:41 +0900

    [core data] : setup UI

--- play_with_coredata/CoreDataPractice/CoreDataPractice/ViewController.swift
+++ play_with_coredata/CoreDataPractice/CoreDataPractice/ViewController.swift
@@ -1,8 +1,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-  let storageProvider: StorageProvider
+  private let movieNameTextField = UITextField()
+  private let saveButton = UIButton(type: .system)
   
+  //MARK: - Properties
+  private let storageProvider: StorageProvider
+  
+  //MARK: - Initializer
   init(storageProvider: StorageProvider) {
     self.storageProvider = storageProvider
     super.init(nibName: nil, bundle: nil)
@@ -14,7 +19,55 @@ class ViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    view.backgroundColor = .systemRed
+    view.backgroundColor = .systemBackground
+    
+    style()
+    layout()
+  }
+  
+  @objc func didTappedSaveButton() {
+    // Do Something
+  }
+}
+
+extension ViewController {
+  private func style() {
+    movieNameTextField.autocorrectionType = .no
+    movieNameTextField.autocapitalizationType = .none
+    movieNameTextField.textColor = .label
+    movieNameTextField.layer.cornerRadius = 8
+    movieNameTextField.layer.borderColor = UIColor.secondaryLabel.cgColor
+    movieNameTextField.layer.borderWidth = 1
+    movieNameTextField.backgroundColor = .systemBackground
+    movieNameTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+    movieNameTextField.leftViewMode = .always
+    
+    saveButton.backgroundColor = .link
+    saveButton.setTitleColor(.white, for: .normal)
+    saveButton.setTitle("  Save  ", for: .normal)
+    saveButton.layer.cornerRadius = 8
+    saveButton.addTarget(self, action: #selector(didTappedSaveButton), for: .touchUpInside)
+  }
+  
+  private func layout() {
+    movieNameTextField.translatesAutoresizingMaskIntoConstraints = false
+    saveButton.translatesAutoresizingMaskIntoConstraints = false
+    
+    view.addSubview(movieNameTextField)
+    view.addSubview(saveButton)
+    
+    NSLayoutConstraint.activate([
+      movieNameTextField.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+      movieNameTextField.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
+      movieNameTextField.heightAnchor.constraint(equalToConstant: 48),
+      movieNameTextField.leadingAnchor.constraint(equalToSystemSpacingAfter: view.safeAreaLayoutGuide.leadingAnchor, multiplier: 1),
+      view.safeAreaLayoutGuide.trailingAnchor.constraint(equalToSystemSpacingAfter: movieNameTextField.trailingAnchor, multiplier: 1),
+      
+      saveButton.topAnchor.constraint(equalToSystemSpacingBelow: movieNameTextField.bottomAnchor, multiplier: 1),
+      saveButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+      saveButton.heightAnchor.constraint(equalToConstant: 32),
+    ])
+    
   }
 }
 
