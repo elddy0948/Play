1fc5875ca9536f8a95cb7e82c47ebfdb395ac447
Joons <elddy0948@gmail.com>
2023-01-25 14:24:36 +0900

    [UIKitCombinePractice] project setup

--- Play with Combine/UIKitCombinePractice/UIKitCombinePractice/ViewController.swift
+++ Play with Combine/UIKitCombinePractice/UIKitCombinePractice/ViewController.swift
@@ -1,19 +1,11 @@
-//
-//  ViewController.swift
-//  UIKitCombinePractice
-//
-//  Created by 김호준 on 2023/01/25.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    view.backgroundColor = .systemGreen
   }
-
-
 }
 
