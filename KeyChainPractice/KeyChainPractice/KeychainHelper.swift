9d428945ae823e64721ffe717c8285ae149edbb9
Joons <elddy0948@gmail.com>
2023-02-02 16:21:54 +0900

    [KeyChainPractice]: update when data duplicate

--- KeyChainPractice/KeyChainPractice/KeychainHelper.swift
+++ KeyChainPractice/KeyChainPractice/KeychainHelper.swift
@@ -17,5 +17,19 @@ final class KeychainHelper {
     if status != errSecSuccess {
       print("Error! : \(status)")
     }
+    
+    // 중복이면 update!
+    if status == errSecDuplicateItem {
+      let query = [
+        kSecAttrService: service,
+        kSecAttrAccount: account,
+        kSecClass: kSecClassGenericPassword
+      ] as CFDictionary
+      
+      let attributesToUpdate = [kSecValueData: data] as CFDictionary
+      
+      //update
+      SecItemUpdate(query, attributesToUpdate)
+    }
   }
 }
